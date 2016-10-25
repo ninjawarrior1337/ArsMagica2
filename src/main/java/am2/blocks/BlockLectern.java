9c475920fd6221b3e1c1d27c88257e4ecd6b0fc3
Growlith1223 <Growlith1223@voltisoft.com>
2016-10-25 09:21:52 -0700

    fix Imports

--- src/main/java/am2/blocks/BlockLectern.java
+++ src/main/java/am2/blocks/BlockLectern.java
@@ -20,7 +20,7 @@ import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraft.world.WorldSettings.GameType;
+import net.minecraft.world.GameType;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
