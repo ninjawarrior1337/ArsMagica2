dfc6a98f70c12efa81be585b3539b1930811a515
Growlith1223 <Growlith1223@voltisoft.com>
2016-12-10 21:31:58 -0800

    Fix Antenna not giving resistance to fire damage
    Fixes #94

--- src/main/java/am2/armor/ItemFireGuardianEars.java
+++ src/main/java/am2/armor/ItemFireGuardianEars.java
@@ -9,10 +9,15 @@ import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.PotionTypes;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.translation.I18n;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -49,5 +54,12 @@ public class ItemFireGuardianEars extends AMArmor{
 	@Override
 	public void getSubItems(Item par1, CreativeTabs par2CreativeTabs, List<ItemStack> par3List){
 		par3List.add(ItemDefs.fireEarsEnchanted.copy());
+	}
+
+	@Override
+	public void onArmorTick(World world, EntityPlayer plr, ItemStack stack) {
+		super.onArmorTick(world, plr, stack);
+		if (!plr.getActivePotionEffects().contains(PotionTypes.FIRE_RESISTANCE))
+			plr.addPotionEffect(new PotionEffect(Potion.getPotionFromResourceLocation(new ResourceLocation("fire_resistance").toString())));
 	}
 }
