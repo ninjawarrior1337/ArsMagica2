96d58b875263cfbf0496721ca761f82974d6d60d
Growlith1223 <Growlith1223@voltisoft.com>
2016-12-24 22:24:59 -0800

    Attempt to fix crash with Obelisk rendering
    This is in v1.5.0-14 and above, just didn't commit till now

--- src/main/java/am2/blocks/render/TileObeliskRenderer.java
+++ src/main/java/am2/blocks/render/TileObeliskRenderer.java
@@ -1,5 +1,6 @@
 package am2.blocks.render;
 
+import net.minecraft.init.Blocks;
 import org.lwjgl.opengl.GL11;
 
 import com.google.common.base.Function;
@@ -59,61 +60,63 @@ public class TileObeliskRenderer extends TileEntitySpecialRenderer<TileEntityObe
 	
 	@Override
 	public void renderTileEntityAt(TileEntityObelisk te, double x, double y, double z, float partialTicks, int destroyStage) {
-		GlStateManager.pushAttrib();
-		GlStateManager.pushMatrix();
-		GlStateManager.translate(x, y, z);
-		GlStateManager.disableRescaleNormal();
-		GlStateManager.pushMatrix();
-		RenderHelper.disableStandardItemLighting();
-		EnumFacing facing = EnumFacing.NORTH;
-		if (te.hasWorldObj()) {
-			IBlockState state = te.getWorld().getBlockState(te.getPos());
-			facing = state.getValue(BlockEssenceGenerator.FACING);
-		}
-		if (facing == EnumFacing.WEST || facing == EnumFacing.SOUTH)
-			GlStateManager.translate(0, 0, 1);
-		if (facing == EnumFacing.SOUTH || facing == EnumFacing.EAST)
-			GlStateManager.translate(1, 0, 0);
-		GlStateManager.rotate(180 - facing.getHorizontalAngle(), 0, 1, 0);
-		GlStateManager.translate(-te.getPos().getX(), -te.getPos().getY(), -te.getPos().getZ());
-		if (Minecraft.isAmbientOcclusionEnabled())
-			GlStateManager.shadeModel(GL11.GL_SMOOTH);
-		else
-			GlStateManager.shadeModel(GL11.GL_FLAT);
-		Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
-		Tessellator tessellator = Tessellator.getInstance();
-		tessellator.getBuffer().begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
-		if (te.hasWorldObj())
-			Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelRenderer().renderModel(te.getWorld(), getBakedModel(te), te.getWorld().getBlockState(te.getPos()), te.getPos(), tessellator.getBuffer(), false);
-		else
-			Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelRenderer().renderModel(Minecraft.getMinecraft().theWorld, getBakedModel(te), BlockDefs.obelisk.getDefaultState(), new BlockPos(0, 0, 0), tessellator.getBuffer(), false);
-		tessellator.draw();
-		if (te.isActive()) {
-			GL11.glMatrixMode(GL11.GL_TEXTURE);
-			GL11.glPushMatrix();
-			GL11.glLoadIdentity();
-			GlStateManager.enableBlend();
-			TextureAtlasSprite sprite = Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite("arsmagica2:blocks/custom/obelisk_runes");
-			GlStateManager.scale(1/(sprite.getMaxU() - sprite.getMinU()), 1/(sprite.getMaxV() - sprite.getMinV()), 1);
-			Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("arsmagica2:textures/blocks/custom/obelisk_runes.png"));
-			tessellator.getBuffer().begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
-			float normx = (System.currentTimeMillis() % 32000) / 32000.0f;
-			float normy = (System.currentTimeMillis() % 28000) / 28000.0f;
-			GL11.glTranslatef(normx, normy, 0);
-			float transp = (float)Math.abs(Math.sin(System.currentTimeMillis() / 1000.0));
-			GL11.glColor4f(1, 1, 1, transp);
-			Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelRenderer().renderModel(te.getWorld(), runesBakedModel, te.getWorld().getBlockState(te.getPos()), te.getPos(), tessellator.getBuffer(), false);
-			tessellator.draw();
-			GL11.glPopMatrix();
-			GlStateManager.disableBlend();
-			GL11.glMatrixMode(GL11.GL_MODELVIEW);
-		}
-		
-		RenderHelper.enableStandardItemLighting();
-		GlStateManager.popMatrix();
-		GlStateManager.enableRescaleNormal();
-		GlStateManager.popMatrix();
-		GlStateManager.popAttrib();
+        if (!te.getWorld().isBlockLoaded(te.getPos(), false) || te.getWorld().getBlockState(te.getPos()).getBlock() != BlockDefs.obelisk)
+            return;
+        GlStateManager.pushAttrib();
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(x, y, z);
+        GlStateManager.disableRescaleNormal();
+        GlStateManager.pushMatrix();
+        RenderHelper.disableStandardItemLighting();
+        EnumFacing facing = EnumFacing.NORTH;
+        if (te.hasWorldObj()) {
+            IBlockState state = te.getWorld().getBlockState(te.getPos());
+            facing = state.getValue(BlockEssenceGenerator.FACING);
+        }
+        if (facing == EnumFacing.WEST || facing == EnumFacing.SOUTH)
+            GlStateManager.translate(0, 0, 1);
+        if (facing == EnumFacing.SOUTH || facing == EnumFacing.EAST)
+            GlStateManager.translate(1, 0, 0);
+        GlStateManager.rotate(180 - facing.getHorizontalAngle(), 0, 1, 0);
+        GlStateManager.translate(-te.getPos().getX(), -te.getPos().getY(), -te.getPos().getZ());
+        if (Minecraft.isAmbientOcclusionEnabled())
+            GlStateManager.shadeModel(GL11.GL_SMOOTH);
+        else
+            GlStateManager.shadeModel(GL11.GL_FLAT);
+        Minecraft.getMinecraft().renderEngine.bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
+        Tessellator tessellator = Tessellator.getInstance();
+        tessellator.getBuffer().begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
+        if (te.hasWorldObj())
+            Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelRenderer().renderModel(te.getWorld(), getBakedModel(te), te.getWorld().getBlockState(te.getPos()), te.getPos(), tessellator.getBuffer(), false);
+        else
+            Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelRenderer().renderModel(Minecraft.getMinecraft().theWorld, getBakedModel(te), BlockDefs.obelisk.getDefaultState(), new BlockPos(0, 0, 0), tessellator.getBuffer(), false);
+        tessellator.draw();
+        if (te.isActive()) {
+            GL11.glMatrixMode(GL11.GL_TEXTURE);
+            GL11.glPushMatrix();
+            GL11.glLoadIdentity();
+            GlStateManager.enableBlend();
+            TextureAtlasSprite sprite = Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite("arsmagica2:blocks/custom/obelisk_runes");
+            GlStateManager.scale(1/(sprite.getMaxU() - sprite.getMinU()), 1/(sprite.getMaxV() - sprite.getMinV()), 1);
+            Minecraft.getMinecraft().renderEngine.bindTexture(new ResourceLocation("arsmagica2:textures/blocks/custom/obelisk_runes.png"));
+            tessellator.getBuffer().begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
+            float normx = (System.currentTimeMillis() % 32000) / 32000.0f;
+            float normy = (System.currentTimeMillis() % 28000) / 28000.0f;
+            GL11.glTranslatef(normx, normy, 0);
+            float transp = (float)Math.abs(Math.sin(System.currentTimeMillis() / 1000.0));
+            GL11.glColor4f(1, 1, 1, transp);
+            Minecraft.getMinecraft().getBlockRendererDispatcher().getBlockModelRenderer().renderModel(te.getWorld(), runesBakedModel, te.getWorld().getBlockState(te.getPos()), te.getPos(), tessellator.getBuffer(), false);
+            tessellator.draw();
+            GL11.glPopMatrix();
+            GlStateManager.disableBlend();
+            GL11.glMatrixMode(GL11.GL_MODELVIEW);
+        }
+
+        RenderHelper.enableStandardItemLighting();
+        GlStateManager.popMatrix();
+        GlStateManager.enableRescaleNormal();
+        GlStateManager.popMatrix();
+        GlStateManager.popAttrib();
 	}
 
 	private IBakedModel getBakedModel(TileEntityObelisk obelisk) {
